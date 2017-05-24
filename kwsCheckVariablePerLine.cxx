msmolens/KWStyle

commit 1c2b4242bbf0cfd419ea9ddd6283da528a640645

ENH: Reverting to using long int instead of size_t

kwsCheckVariablePerLine.cxx
@@ -32,19 +32,19 @@ bool Parser::CheckVariablePerLine(unsigned long max)
    "char","unsigned char",
    "short","unsigned short",
    "long","unsigned long",
-   "float","double","void","size_t"};
+   "float","double","void","long int"};
 
   bool hasError = false;
   for(unsigned int i = 0;i<ntypes;i++)
     {
     std::string typeToFind = types[i];
     typeToFind += " ";
-    size_t posType = m_BufferNoComment.find(typeToFind,0);
-    while(posType != -1)
+    long int posType = m_BufferNoComment.find(typeToFind,0);
+    while(posType != std::string::npos)
       {
       // Check that this is the first word
       bool firstWord = false;
-      size_t pos=posType;
+      long int pos=posType;
       pos--;
       while((pos>0) && (m_BufferNoComment[pos]==' '))
         {
@@ -90,13 +90,13 @@ bool Parser::CheckVariablePerLine(unsigned long max)
             {
             // Check that we are not initializing an array
             bool betweenBraces = false;           
-            size_t openCurly = pos-1;
+            long int openCurly = pos-1;
             while(openCurly>0)
               {
               // Ok we have the opening
               if(line[openCurly] == '{')
                 {
-                size_t posClosing = this->FindClosingChar('{','}',openCurly,false,line);
+                long int posClosing = this->FindClosingChar('{','}',openCurly,false,line);
                 if(posClosing == -1
                   || pos<posClosing)
                   {

kwsCheckBadCharacters.cxx
@@ -31,12 +31,12 @@ bool Parser::CheckBadCharacters(bool checkComments)
   
   unsigned long pos = 0;
   std::string::const_iterator it = buffer.begin();
-  size_t currentline = -1;
+  long int currentline = -1;
   while(it!= buffer.end())
     {
     if((*it)<0) 
       {
-      size_t linenum = this->GetLineNumber(pos,!checkComments);
+      long int linenum = this->GetLineNumber(pos,!checkComments);
       if(linenum != currentline)
         {
         currentline = linenum;

kwsCheckBlackList.cxx
@@ -52,22 +52,36 @@ bool Parser::CheckBlackList(const char* filename)
     }
 
   // Go through the list of words
-  size_t start = 0;
-  size_t space = m_BlackListBuffer.find(" ",start);
-  size_t eol = m_BlackListBuffer.find("\n",start);
+  std::string::size_type start = 0;
+  std::string::size_type space = m_BlackListBuffer.find(" ",start);
+  std::string::size_type eol = m_BlackListBuffer.find("\n",start);
 
-  size_t end = space;
-  if(eol != -1 && eol < space)
+  std::string::size_type end = space;
+  if(eol != std::string::npos && eol < space)
     {
-    end = eol-1;
+    if(eol == 0)
+      {
+      end = std::string::npos;
+      }
+    else
+      {
+      end = eol-1;
+      }
     }
 
-  if(end == -1 && eol != -1)
+  if(end == std::string::npos && eol != std::string::npos)
     {
-    end = eol-1;
+    if(eol == 0)
+      {
+      end = std::string::npos;
+      }
+    else
+      {
+      end = eol-1;
+      }
     }
 
-  while(end != -1 && end>start)
+  while(end != std::string::npos && end>start)
     {
     std::string blackword = m_BlackListBuffer.substr(start,end-start);
     start = end+1;
@@ -76,13 +90,27 @@ bool Parser::CheckBlackList(const char* filename)
     eol = m_BlackListBuffer.find("\n",start);
 
     end = space;
-    if(eol != -1 && eol < space)
+    if(eol != std::string::npos && eol < space)
       {
-      end = eol-1;
+      if(eol == 0)
+        {
+        end = std::string::npos;
+        }
+      else
+        {
+        end = eol-1;
+        }
       }
-    if(end == -1 && eol != -1)
+    if(end == std::string::npos && eol != std::string::npos)
       {
-      end = eol-1;
+      if(eol == 0)
+        {
+        end = std::string::npos;
+        }
+      else
+        {
+        end = eol-1;
+        }
       }
 
     if(blackword.length() <= 1)
@@ -90,8 +118,8 @@ bool Parser::CheckBlackList(const char* filename)
       continue;
       }
 
-    size_t pos = m_BufferNoComment.find(blackword,0);
-    while(pos != -1)
+    std::string::size_type pos = m_BufferNoComment.find(blackword,0);
+    while(pos != std::string::npos)
       {
       Error error;
       error.line = this->GetLineNumber(pos,true);

kwsCheckComments.cxx
@@ -40,11 +40,11 @@ bool Parser::CheckComments(const char* begin,const char* middle,const char* end,
     while(it != m_CommentPositions.end())
       {
       std::string previousWord = "";
-      size_t i = (*it).first;
-      while((i<(size_t)m_Buffer.size()) && i<(*it).second)
+      long int i = (*it).first;
+      while((i<(long int)m_Buffer.size()) && i<(*it).second)
         {    
         // we go to the next space
-        while((i<(size_t)m_Buffer.size()) && ((m_Buffer[i] != ' ') && (m_Buffer[i] != '\r') && (m_Buffer[i] != '\r')) && i<(*it).second)
+        while((i<(long int)m_Buffer.size()) && ((m_Buffer[i] != ' ') && (m_Buffer[i] != '\r') && (m_Buffer[i] != '\r')) && i<(*it).second)
           {
           i++;
           }
@@ -52,7 +52,7 @@ bool Parser::CheckComments(const char* begin,const char* middle,const char* end,
         bool first = false;
         std::string word = "";
         
-        while((i<(size_t)m_Buffer.size()) && i<(*it).second && inWord)
+        while((i<(long int)m_Buffer.size()) && i<(*it).second && inWord)
           {
           if(m_Buffer[i] != ' ' && m_Buffer[i] != '\r' && m_Buffer[i] != '\n')
             {
@@ -112,10 +112,10 @@ bool Parser::CheckComments(const char* begin,const char* middle,const char* end,
       m_TestsDescription[MISSINGCOMMENT] += " (not allowing empty line)";
       }
 
-   size_t pos = this->GetClassPosition(0);
+   long int pos = this->GetClassPosition(0);
    while(pos  != -1)
      {
-     size_t poswithcom = this->GetPositionWithComments(pos);
+     long int poswithcom = this->GetPositionWithComments(pos);
 
      // Find the last comment (remove spaces if any)
      std::string commentEnd = "";
@@ -127,8 +127,8 @@ bool Parser::CheckComments(const char* begin,const char* middle,const char* end,
          }
        }
 
-     size_t poscom = m_Buffer.find(commentEnd,0);
-     size_t poscomtemp = poscom;
+     long int poscom = m_Buffer.find(commentEnd,0);
+     long int poscomtemp = poscom;
      while(poscomtemp!=-1 && poscomtemp<poswithcom)
        {
        poscom = poscomtemp;
@@ -152,7 +152,7 @@ bool Parser::CheckComments(const char* begin,const char* middle,const char* end,
        // We check if we have m_CommentEnd before an empty line
        bool emptyLine = false;
        bool gotchar = true;
-       for(size_t i=poscom;i<poswithcom;i++)
+       for(long int i=poscom;i<poswithcom;i++)
          {
          if(m_Buffer[i] == '\n')
            {
@@ -188,8 +188,8 @@ bool Parser::CheckComments(const char* begin,const char* middle,const char* end,
        else  // we check that the word \class exists
          {
          // Find the last 
-         size_t poscombeg = m_Buffer.find(m_CommentBegin,0);
-         size_t poscombegt = poscombeg;
+         long int poscombeg = m_Buffer.find(m_CommentBegin,0);
+         long int poscombegt = poscombeg;
          while(poscombegt!=-1 && poscombegt<poscom)
            {
            poscombeg = poscombegt;

kwsCheckDeclarationOrder.cxx
@@ -43,12 +43,12 @@ bool Parser::CheckDeclarationOrder(unsigned int posPublic, unsigned int posProte
 
   delete [] val;
 
-  size_t publicFirst;
-  size_t publicLast;
+  long int publicFirst;
+  long int publicLast;
   this->FindPublicArea(publicFirst,publicLast);
   
-  size_t class_end = this->FindEndOfClass(publicFirst);
-  size_t class_beg = this->FindOpeningChar('}','{',class_end,true);
+  long int class_end = this->FindEndOfClass(publicFirst);
+  long int class_beg = this->FindOpeningChar('}','{',class_end,true);
 
   // Find the first public declaration
   while(publicFirst!=-1 && publicFirst!=MAX_CHAR && (publicFirst<class_beg || publicFirst>class_end))
@@ -59,10 +59,10 @@ bool Parser::CheckDeclarationOrder(unsigned int posPublic, unsigned int posProte
     }
   
   // Currently checking only one class per file (do a loop in the future)
-  size_t currentclass = class_beg;
+  long int currentclass = class_beg;
 
-  size_t protectedFirst;
-  size_t protectedLast;
+  long int protectedFirst;
+  long int protectedLast;
   this->FindProtectedArea(protectedFirst,protectedLast);
 
   class_end = this->FindEndOfClass(protectedFirst);
@@ -74,8 +74,8 @@ bool Parser::CheckDeclarationOrder(unsigned int posPublic, unsigned int posProte
     class_beg = this->FindOpeningChar('}','{',class_end,true);
     }
   
-  size_t privateFirst;
-  size_t privateLast;
+  long int privateFirst;
+  long int privateLast;
   this->FindPrivateArea(privateFirst,privateLast);
 
   class_end = this->FindEndOfClass(privateFirst);

kwsCheckExtraSpaces.cxx
@@ -27,7 +27,7 @@ bool Parser::CheckExtraSpaces(unsigned long max,bool checkEmptyLines)
   delete [] val;
 
   bool hasError = false;
-  size_t posEndOfLine = m_Buffer.find("\r\n",0);
+  long int posEndOfLine = m_Buffer.find("\r\n",0);
   while(posEndOfLine != -1)
     {
     bool checking = true;

kwsCheckFunctions.cxx
@@ -90,8 +90,8 @@ bool Parser::CheckFunctions(const char* regEx,unsigned long maxLength)
        && functionName.find("~") == std::string::npos // skip destructor for now...
        )
       {
-      size_t posf = functionName.find("::",0);
-      size_t posp = functionName.find("(",posf);
+      long int posf = functionName.find("::",0);
+      long int posp = functionName.find("(",posf);
       if(posp != -1 && posf != -1 && posp>posf)
         {
         functionName = functionName.substr(posf+2,posp-posf-2);
@@ -110,7 +110,7 @@ bool Parser::CheckFunctions(const char* regEx,unsigned long maxLength)
 
     if(functionName.size() == 0)
       {
-      size_t bf = m_BufferNoComment.find('{',pos);
+      long int bf = m_BufferNoComment.find('{',pos);
       long int pos2 = this->FindClosingChar('{','}',bf,true);
       pos = this->FindFunction(pos2+1);
 
@@ -124,7 +124,7 @@ bool Parser::CheckFunctions(const char* regEx,unsigned long maxLength)
       }
     else if(functionName.size()>0)
       {
-      size_t bf = m_BufferNoComment.find('{',pos);
+      long int bf = m_BufferNoComment.find('{',pos);
       long int bfcomments = GetPositionWithComments(bf);
       long int bfl = this->GetLineNumber(bfcomments);
       long int pos2 = this->FindClosingChar('{','}',bf,true);
@@ -136,7 +136,7 @@ bool Parser::CheckFunctions(const char* regEx,unsigned long maxLength)
       // we cannot go backward
       if(pos2 > pos)
         {
-        size_t bf = m_BufferNoComment.find('{',pos2);
+        long int bf = m_BufferNoComment.find('{',pos2);
         pos = this->FindFunction(bf);
         }
      

kwsCheckHeader.cxx
@@ -155,11 +155,11 @@ bool Parser::CheckHeader(const char* filename, bool considerSpaceEOL,bool useCVS
         // Check if we have a <NA> tag
         if((*ith) == '<')
           {
-          size_t pos2 = buffer.find("<NA>",posh);
-          size_t pos3 = buffer.find("<NA>\n",posh);
+          long int pos2 = buffer.find("<NA>",posh);
+          long int pos3 = buffer.find("<NA>\n",posh);
 
           // We skip the line
-          if(pos3 == static_cast<size_t>(posh))
+          if(pos3 == static_cast<long int>(posh))
             {
             while(((*ith) != '\n') && (ith != buffer.end()))
               {
@@ -174,7 +174,7 @@ bool Parser::CheckHeader(const char* filename, bool considerSpaceEOL,bool useCVS
             continue;
             }
           // if we have the tag we skip the word
-          else if(pos2 == static_cast<size_t>(posh))
+          else if(pos2 == static_cast<long int>(posh))
             {
             while(((*ith) != ' ') && (ith != buffer.end()))
               {
@@ -249,8 +249,8 @@ bool Parser::CheckHeader(const char* filename, bool considerSpaceEOL,bool useCVS
           {
           line = l;
           // Find the word
-          size_t poshw = buffer.find(' ',posh);
-          size_t poshw2 = buffer.find('\n',posh);
+          long int poshw = buffer.find(' ',posh);
+          long int poshw2 = buffer.find('\n',posh);
           std::string wordh = "";
           if(poshw!= -1 && poshw < poshw2)
             {
@@ -262,8 +262,8 @@ bool Parser::CheckHeader(const char* filename, bool considerSpaceEOL,bool useCVS
             }
 
           // Find the word
-          size_t posw = m_Buffer.find(' ',pos);
-          size_t posw2 = m_Buffer.find('\n',pos);
+          long int posw = m_Buffer.find(' ',pos);
+          long int posw2 = m_Buffer.find('\n',pos);
           std::string word = "";
           if(posw!=-1 && posw < posw2)
             {

commit 3f5ff5c79acce47e7c97a99fc39f7fe3d90f297f

Fix checking the number of statements per line for size_t
Fix a regression in checking the number of statements per line for variables
declared as size_t.

This regressed in commit 1c2b4242bbf0cfd419ea9ddd6283da528a640645. Following
that commit, Test5031 failed, but the test failure was masked by a regression
introduced in commit 8216a1670c25d0b00856ad23fb0f23f6f5f4a631.

kwsCheckVariablePerLine.cxx
@@ -32,7 +32,7 @@ bool Parser::CheckVariablePerLine(unsigned long max)
    "char","unsigned char",
    "short","unsigned short",
    "long","unsigned long",
-   "float","double","void","long int"};
+   "float","double","void","size_t"};
 
   bool hasError = false;
   for(unsigned int i = 0;i<ntypes;i++)